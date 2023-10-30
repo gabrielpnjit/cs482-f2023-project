5130c438f8bc1b16c76da9c3e0a4450688b6315a
Gabriel Pascual <gzp2@njit.edu>
2023-10-29 23:17:06 -0400

    fixed python script to only download videos with captions and also handle age restricted videos. Downloaded 50 vidoes and captions

--- download_yt.py
+++ download_yt.py
@@ -21,21 +21,28 @@ videos_output_directory = os.path.join(current_directory, 'videos')
 captions_output_directory = os.path.join(current_directory, 'captions')
 
 print(f'Downloading videos by: {c.channel_name}')
-for i in range(50):
+
+i = 0
+count = 50
+vid_count = 0
+while i < len(c.videos) and vid_count < count:
 	# have to use this method before accessing other properties like captions
 	# https://github.com/pytube/pytube/issues/1674#issuecomment-1706105785
 	# save videos
 	video = c.videos[i]
-	video.streams.first().download(output_path=videos_output_directory)
-	video.bypass_age_gate()
-
-	# save captions
+	# try catch block handles age restricted and non captioned videos
 	try:
+		video.bypass_age_gate()
 		caption = video.captions['en']
+		video.streams.first().download(output_path=videos_output_directory)
+		print(f'Video #{vid_count} - Downloading {video.title}')
 		caption_content = caption.generate_srt_captions()
 		caption_filename = f"{sanitize_filename(video.title)}.srt"
 		caption_path = os.path.join(captions_output_directory, caption_filename)
 		with open(caption_path, 'w', encoding='utf-8') as file:
 			file.write(caption_content)
+		vid_count += 1
+		i += 1
 	except:
+		i += 1
 		continue
