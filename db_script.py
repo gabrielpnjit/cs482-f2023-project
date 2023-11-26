0f4bc8eab2035db0ffca4c43c4eeaff02c5b881b
Gabriel Pascual <gzp2@njit.edu>
2023-11-26 02:36:23 -0500

    text embeddings insert works

--- db_script.py
+++ db_script.py
@@ -1,5 +1,14 @@
 import numpy as np
 import psycopg2
+import gensim.downloader as api
+from gensim.models import Word2Vec
+from nltk.tokenize import word_tokenize
+import nltk
+from nltk.corpus import stopwords
+import re
+
+nltk.download('stopwords')
+nltk.download('punkt')
 
 # postgres db params
 dbname = 'test_db'
@@ -25,6 +34,42 @@ def insert_embedding(file_path, video_title, frame_number):
     cur.close()
     conn.close()
 
-for i in range(50):
-	insert_embedding(f'video_embeddings/2nd Batch Of Aid Reaches Gaza As Israeli Air Strikes Intensify  NPR News Now/embeddings/embedding{i}.npy', '2nd Batch Of Aid Reaches Gaza As Israeli Air Strikes Intensify  NPR News Now', i)
+def preprocess_text(text):
+    text = re.sub(r'\[.*?\]', '', text)
+    text = re.sub(r'\W+|\d+', ' ', text)
+    tokens = word_tokenize(text)
+    tokens = [word.lower() for word in tokens]
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+    return tokens
 
+model = api.load('word2vec-google-news-300')
+
+def insert_text_embedding(file_path, video_title):
+    # Load text
+    file = open(file_path, 'r', encoding='utf-8')
+    text = file.read()
+    file.close()
+
+    # word2vec embedding
+    tokens = preprocess_text(text)
+    embeddings = [model[word] for word in tokens if word in model]
+    sentence_embedding = sum(embeddings) / len(embeddings)
+    sentence_embedding = sentence_embedding.tolist()
+    # Insert into the database
+    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO text_embeddings (video_title, embedding) VALUES (%s, %s)", 
+                (video_title, sentence_embedding,))
+    conn.commit()
+
+    # Close the connection
+    cur.close()
+    conn.close()
+
+insert_text_embedding('captions/2,000 Bodies Recovered After Dam Bursts In Derna, Libya _ NPR News Now.srt', '2,000 Bodies Recovered After Dam Bursts In Derna, Libya _ NPR News Now')
+# insert video frame embeddings
+# for i in range(50):
+# 	insert_embedding(f'video_embeddings/2nd Batch Of Aid Reaches Gaza As Israeli Air Strikes Intensify  NPR News Now/embeddings/embedding{i}.npy', '2nd Batch Of Aid Reaches Gaza As Israeli Air Strikes Intensify  NPR News Now', i)
+
+# insert video caption embeddings
