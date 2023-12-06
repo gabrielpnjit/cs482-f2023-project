0503b4feb84647e14f4afd2025ec983b648b08a5
Gabriel Pascual <gzp2@njit.edu>
2023-12-06 02:22:46 -0500

    milestone 4 work, had to copy into another folder and commit at once because issues with git lfs

---
+++ dual_encoder.py
@@ -0,0 +1,309 @@
+import os
+import collections
+import json
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_hub as hub
+import tensorflow_text as text
+import tensorflow_addons as tfa
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from tqdm import tqdm # this is progress bar in console
+
+# Suppressing tf.hub warnings
+tf.get_logger().setLevel("ERROR")
+
+root_dir = "datasets"
+annotations_dir = os.path.join(root_dir, "captions")
+images_dir = os.path.join(root_dir, "images")
+tfrecords_dir = os.path.join(root_dir, "tfrecords")
+annotation_file = os.path.join(annotations_dir, "captions.txt")
+
+annotations = []
+with open(annotation_file, 'r') as file:
+    for i, line in enumerate(file):
+        if i == 0:
+            continue
+        image, caption = line.strip().split(',', 1)
+        annotations.append({'image': image, 'caption': caption})
+
+image_path_to_caption = collections.defaultdict(list)
+for element in annotations:
+    caption = f"{element['caption'].lower().rstrip('.')}"
+    image_path = f"{images_dir}/{(element['image'])}"
+    image_path_to_caption[image_path].append(caption)
+
+image_paths = list(image_path_to_caption.keys())
+print(f"Number of images: {len(image_paths)}")
+
+train_size = 5000
+valid_size = 1000
+captions_per_image = 1
+images_per_file = 100
+
+train_image_paths = image_paths[:train_size]
+num_train_files = int(np.ceil(train_size / images_per_file))
+train_files_prefix = os.path.join(tfrecords_dir, "train")
+
+valid_image_paths = image_paths[-valid_size:]
+num_valid_files = int(np.ceil(valid_size / images_per_file))
+valid_files_prefix = os.path.join(tfrecords_dir, "valid")
+
+tf.io.gfile.makedirs(tfrecords_dir)
+
+
+def bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def create_example(image_path, caption):
+    feature = {
+        "caption": bytes_feature(caption.encode()),
+        "raw_image": bytes_feature(tf.io.read_file(image_path).numpy()),
+    }
+    return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
+def write_tfrecords(file_name, image_paths):
+    caption_list = []
+    image_path_list = []
+    for image_path in image_paths:
+        captions = image_path_to_caption[image_path][:captions_per_image]
+        caption_list.extend(captions)
+        image_path_list.extend([image_path] * len(captions))
+
+    with tf.io.TFRecordWriter(file_name) as writer:
+        for example_idx in range(len(image_path_list)):
+            example = create_example(
+                image_path_list[example_idx], caption_list[example_idx]
+            )
+            writer.write(example.SerializeToString())
+    return example_idx + 1
+
+
+def write_data(image_paths, num_files, files_prefix):
+    example_counter = 0
+    for file_idx in tqdm(range(num_files)):
+        file_name = files_prefix + "-%02d.tfrecord" % (file_idx)
+        start_idx = images_per_file * file_idx
+        end_idx = start_idx + images_per_file
+        example_counter += write_tfrecords(file_name, image_paths[start_idx:end_idx])
+    return example_counter
+
+
+train_example_count = write_data(train_image_paths, num_train_files, train_files_prefix)
+print(f"{train_example_count} training examples were written to tfrecord files.")
+
+valid_example_count = write_data(valid_image_paths, num_valid_files, valid_files_prefix)
+print(f"{valid_example_count} evaluation examples were written to tfrecord files.")
+
+feature_description = {
+    "caption": tf.io.FixedLenFeature([], tf.string),
+    "raw_image": tf.io.FixedLenFeature([], tf.string),
+}
+
+
+def read_example(example):
+    features = tf.io.parse_single_example(example, feature_description)
+    raw_image = features.pop("raw_image")
+    features["image"] = tf.image.resize(
+        tf.image.decode_jpeg(raw_image, channels=3), size=(299, 299)
+    )
+    return features
+
+
+def get_dataset(file_pattern, batch_size):
+
+    return (
+        tf.data.TFRecordDataset(tf.data.Dataset.list_files(file_pattern))
+        .map(
+            read_example,
+            num_parallel_calls=tf.data.AUTOTUNE,
+            deterministic=False,
+        )
+        .shuffle(batch_size * 10)
+        .prefetch(buffer_size=tf.data.AUTOTUNE)
+        .batch(batch_size)
+    )
+
+
+def project_embeddings(
+    embeddings, num_projection_layers, projection_dims, dropout_rate
+):
+    projected_embeddings = layers.Dense(units=projection_dims)(embeddings)
+    for _ in range(num_projection_layers):
+        x = tf.nn.gelu(projected_embeddings)
+        x = layers.Dense(projection_dims)(x)
+        x = layers.Dropout(dropout_rate)(x)
+        x = layers.Add()([projected_embeddings, x])
+        projected_embeddings = layers.LayerNormalization()(x)
+    return projected_embeddings
+
+def create_vision_encoder(
+    num_projection_layers, projection_dims, dropout_rate, trainable=False
+):
+    # Load the pre-trained Xception model to be used as the base encoder.
+    xception = keras.applications.Xception(
+        include_top=False, weights="imagenet", pooling="avg"
+    )
+    # Set the trainability of the base encoder.
+    for layer in xception.layers:
+        layer.trainable = trainable
+    # Receive the images as inputs.
+    inputs = layers.Input(shape=(299, 299, 3), name="image_input")
+    # Preprocess the input image.
+    xception_input = tf.keras.applications.xception.preprocess_input(inputs)
+    # Generate the embeddings for the images using the xception model.
+    embeddings = xception(xception_input)
+    # Project the embeddings produced by the model.
+    outputs = project_embeddings(
+        embeddings, num_projection_layers, projection_dims, dropout_rate
+    )
+    # Create the vision encoder model.
+    return keras.Model(inputs, outputs, name="vision_encoder")
+
+def create_text_encoder(
+    num_projection_layers, projection_dims, dropout_rate, trainable=False
+):
+    # Load the BERT preprocessing module.
+    preprocess = hub.KerasLayer(
+        "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2",
+        name="text_preprocessing",
+    )
+    # Load the pre-trained BERT model to be used as the base encoder.
+    bert = hub.KerasLayer(
+        "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1",
+        trainable=trainable
+    )
+    # Set the trainability of the base encoder.
+    bert.trainable = trainable
+    # Receive the text as inputs.
+    inputs = layers.Input(shape=(), dtype=tf.string, name="text_input")
+    # Preprocess the text.
+    bert_inputs = preprocess(inputs)
+    # Generate embeddings for the preprocessed text using the BERT model.
+    embeddings = bert(bert_inputs)["pooled_output"]
+    # Project the embeddings produced by the model.
+    outputs = project_embeddings(
+        embeddings, num_projection_layers, projection_dims, dropout_rate
+    )
+    # Create the text encoder model.
+    return keras.Model(inputs, outputs, name="text_encoder")
+
+class DualEncoder(keras.Model):
+    def __init__(self, text_encoder, image_encoder, temperature=1.0, **kwargs):
+        super().__init__(**kwargs)
+        self.text_encoder = text_encoder
+        self.image_encoder = image_encoder
+        self.temperature = temperature
+        self.loss_tracker = keras.metrics.Mean(name="loss")
+
+    @property
+    def metrics(self):
+        return [self.loss_tracker]
+
+    def call(self, features, training=False):
+        # Place each encoder on a separate GPU (if available).
+        # TF will fallback on available devices if there are fewer than 2 GPUs.
+        with tf.device("/gpu:0"):
+            # Get the embeddings for the captions.
+            caption_embeddings = text_encoder(features["caption"], training=training)
+        with tf.device("/gpu:1"):
+            # Get the embeddings for the images.
+            image_embeddings = vision_encoder(features["image"], training=training)
+        return caption_embeddings, image_embeddings
+
+    def compute_loss(self, caption_embeddings, image_embeddings):
+        # logits[i][j] is the dot_similarity(caption_i, image_j).
+        logits = (
+            tf.matmul(caption_embeddings, image_embeddings, transpose_b=True)
+            / self.temperature
+        )
+        # images_similarity[i][j] is the dot_similarity(image_i, image_j).
+        images_similarity = tf.matmul(
+            image_embeddings, image_embeddings, transpose_b=True
+        )
+        # captions_similarity[i][j] is the dot_similarity(caption_i, caption_j).
+        captions_similarity = tf.matmul(
+            caption_embeddings, caption_embeddings, transpose_b=True
+        )
+        # targets[i][j] = avarage dot_similarity(caption_i, caption_j) and dot_similarity(image_i, image_j).
+        targets = keras.activations.softmax(
+            (captions_similarity + images_similarity) / (2 * self.temperature)
+        )
+        # Compute the loss for the captions using crossentropy
+        captions_loss = keras.losses.categorical_crossentropy(
+            y_true=targets, y_pred=logits, from_logits=True
+        )
+        # Compute the loss for the images using crossentropy
+        images_loss = keras.losses.categorical_crossentropy(
+            y_true=tf.transpose(targets), y_pred=tf.transpose(logits), from_logits=True
+        )
+        # Return the mean of the loss over the batch.
+        return (captions_loss + images_loss) / 2
+
+    def train_step(self, features):
+        with tf.GradientTape() as tape:
+            # Forward pass
+            caption_embeddings, image_embeddings = self(features, training=True)
+            loss = self.compute_loss(caption_embeddings, image_embeddings)
+        # Backward pass
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        # Monitor loss
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    def test_step(self, features):
+        caption_embeddings, image_embeddings = self(features, training=False)
+        loss = self.compute_loss(caption_embeddings, image_embeddings)
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+num_epochs = 5  # In practice, train for at least 30 epochs
+batch_size = 32
+
+vision_encoder = create_vision_encoder(
+    num_projection_layers=1, projection_dims=256, dropout_rate=0.1
+)
+text_encoder = create_text_encoder(
+    num_projection_layers=1, projection_dims=256, dropout_rate=0.1
+)
+dual_encoder = DualEncoder(text_encoder, vision_encoder, temperature=0.05)
+dual_encoder.compile(
+    optimizer=tfa.optimizers.AdamW(learning_rate=0.001, weight_decay=0.001)
+)
+
+print(f"Number of GPUs: {len(tf.config.list_physical_devices('GPU'))}")
+print(f"Number of examples (caption-image pairs): {train_example_count}")
+print(f"Batch size: {batch_size}")
+print(f"Steps per epoch: {int(np.ceil(train_example_count / batch_size))}")
+train_dataset = get_dataset(os.path.join(tfrecords_dir, "train-*.tfrecord"), batch_size)
+valid_dataset = get_dataset(os.path.join(tfrecords_dir, "valid-*.tfrecord"), batch_size)
+# Create a learning rate scheduler callback.
+reduce_lr = keras.callbacks.ReduceLROnPlateau(
+    monitor="val_loss", factor=0.2, patience=3
+)
+# Create an early stopping callback.
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor="val_loss", patience=5, restore_best_weights=True
+)
+history = dual_encoder.fit(
+    train_dataset,
+    epochs=num_epochs,
+    validation_data=valid_dataset,
+    callbacks=[reduce_lr, early_stopping],
+)
+print("Training completed. Saving vision and text encoders...")
+vision_encoder.save("vision_encoder")
+text_encoder.save("text_encoder")
+print("Models are saved.")
+
+plt.plot(history.history["loss"])
+plt.plot(history.history["val_loss"])
+plt.ylabel("Loss")
+plt.xlabel("Epoch")
+plt.legend(["train", "valid"], loc="upper right")
+plt.show()
